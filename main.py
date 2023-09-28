7149b013bb338ae769829d51f58abc6ac564dc5f
MrKolman <alfred.kallberg@elev.ga.ntig.se>
2023-09-28 20:04:46 +0200

    yeyyyye

---
+++ main.py
@@ -0,0 +1,17 @@
+number = int(input("Vad ska ditt värde vara?"))
+counter = 0
+
+while True:
+    
+    counter = counter + 1
+    if(number %2) == 0:
+        number == number/2
+    
+    else:
+        number == number*3+1
+
+    elif:
+        number != 1
+
+
+print(counter)
