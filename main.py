bf88fff9fd8c4388201e832d2c95bd409567aba4
MrKolman <alfred.kallberg@elev.ga.ntig.se>
2023-10-04 11:04:19 +0200

    fixa counter

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ counter = 0
 while True:
     
     counter += 1
+    trueCounter = counter -1
+
     if(number %2) == 0:
         number = number/2
     
     elif (number == 1):
-        print(Fore.GREEN + "Det är", counter, "steg för att den ska hamna i en loop") 
+        print(Fore.GREEN + "Det är", trueCounter, "steg för att den ska hamna i en loop") 
         break
 
     else:
